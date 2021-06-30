c38fb2d0be27442701a397fca2474ca424e1dfda
John Ross <johnyboy11@hotmail.co.uk>
2021-06-30 12:17:53 +0100

    cleaned up AudioAnalyser

--- src/components/MicAudioAnalyser.js
+++ src/components/MicAudioAnalyser.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import AudioVisualiser from './AudioVisualiser'
 
 
- const MicAudioAnalyser = ({ audioInput }) => {
+ const AudioAnalyser = ({ audioInput }) => {
 
     const [audioData, setAudioData] = useState(new Uint8Array(0))
     
@@ -10,29 +10,27 @@ import AudioVisualiser from './AudioVisualiser'
     
     
     useEffect( () => {
-        // sets audio data to be a Uint8Array which is half as long as the analyser fftSize:
-        // determines the amount of data values available for visualisation
-        // setDataArray(new Uint8Array(analyser.frequencyBinCount)) ;
-        // connect the audio analyser to the source of audio
-        // audioContextSource.connect(analyser);
-        // set the request animation frame Id for use when app dismounts/cancels and calls 
-        // tick
+        // empty request animation frame Id
         let rafId;
+        // creates an audio context
         const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+        // creates analyser node
         const analyser = audioContext.createAnalyser();
+        // Creates a data Array which is half the length of the fftSize
+        // it takes in unsigned integers  
         const dataArray = new Uint8Array(analyser.frequencyBinCount);
-        // const [dataArray, setDataArray] = useState(new Uint8Array());
-        // const [audioData, setAudioData] = useState(new Uint8Array());
-    
         
+        // creates a source variable containing the media stream source
+        const source = audioContext.createMediaStreamSource(audioInput)
+        // connects the audio stream to the analyser node
+        source.connect(analyser)
         const tick = () => {
             // copies wave form data into the dataArray which is passed in as an argument   
             analyser.getByteTimeDomainData(dataArray)
-                
-            // sets audioData to be the value of dataArray
+            // sets audioData to be the value of a copy of dataArray
             console.log("audio data:",dataArray)
             setAudioData([...dataArray])
-            // requests a re-render while calling tick recursively
+            // requests a re-render while calling tick in a recursive loop
             rafId = requestAnimationFrame(tick);
         }
     
@@ -44,22 +42,12 @@ import AudioVisualiser from './AudioVisualiser'
             cancelAnimationFrame(rafId);
         }
 
-    }, [audioInput])
-
-    
-    
-
-    // function componentWillUnmount() {
-    //     cancelAnimationFrame(rafId);
-    //     analyser.disconnect();
-    //     source.disconnect();
-    // }
+    }, [])
 
     return(
         <AudioVisualiser audioData={audioData}/>
-        // <p>Testing broken shit</p>
     )
 
 }
 
-export default MicAudioAnalyser;
+export default AudioAnalyser;
