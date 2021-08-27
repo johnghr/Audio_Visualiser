8d8ee3acf0ade2bd725ad3c60aaa30f8bdb11b7f
Yola Jones <johnyboy11@hotmail.co.uk>
2021-08-27 11:33:43 +0100

    added front end logic for edit button, will refactor and move up to container later

--- client/src/components/TrackInterface/Track.jsx
+++ client/src/components/TrackInterface/Track.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Track = ({
     track, 
@@ -8,32 +8,39 @@ const Track = ({
     deleteTrack
 }) => {
 
-  
+    const [isEditing, setIsEditing] = useState(false)
     // set selected track index to be the index of the clicked track list item
     const handleClick = () => setSelectedTrackIndex(index)
     const handleDelete = () => deleteTrack(track)
+    const handleEdit = () => editTrack(track)
+
+    const editTrack = (track) => {
+        console.log("if you loved me you wouldn't want me to change")
+        if (isEditing === true) {
+            setIsEditing(false)
+        } else {
+            setIsEditing(true)
+        }
+    }
     
     return(
        <div className="track-item-container">
+            {isEditing === false ?
             <li 
                 className={selectedTrackIndex === index ? "playing" : ""} 
                 onClick={handleClick}
             >
                 {track}
+            </li> :
+            <input></input>}
 
-                <button 
-                    onClick={handleDelete}
-                >
-                    <svg className="delete-button"><use href="#delete-icon"/></svg>
-                </button>
+            <button onClick={handleDelete}>
+                <svg className="delete-button"><use href="#delete-icon"/></svg>
+            </button>
 
-                <button
-                    // onClick={handleEdit}
-                >
-                    <svg className="edit-button"><use href="#edit-icon"/></svg>
-                </button>
-            
-            </li>
+            <button onClick={handleEdit}>
+                <svg className="edit-button"><use href="#edit-icon"/></svg>
+            </button>
             
         </div> 
     ) 
